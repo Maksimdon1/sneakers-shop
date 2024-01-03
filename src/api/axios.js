e5ce5c5d202261c91d8f6e38fe204ad0e541d42a
Maksimdon1 <maksimkozyrev056@gmail.com>
2024-01-04 01:20:11 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-	baseURL: 'http://192.168.1.3:3002/server/api', //https://mains-h5w5.onrender.com/server/api    http://192.168.1.4:3002/server/api
+	baseURL: 'https://mains-h5w5.onrender.com/server/api', //https://mains-h5w5.onrender.com/server/api    http://192.168.1.4:3002/server/api
 	withCredentials: true,
 	
 })
