42bbd6b08932c2b8be2b3181170161dd25d8fee7
Maksimdon1 <maksimkozyrev056@gmail.com>
2024-01-28 14:51:58 +0300

    home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,37 +1,45 @@
-import React, {useEffect} from 'react'
-import '../style/home.css'
-import {Swipers} from './swipes' 
-import {Slider} from './slider'
-import Login from './login' 
-import {  Link , useParams , useNavigate} from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import * as types from "../store/types"
-import DefaultButton from './_microComponents/DefaultButton'
-export  function Home() {
-    const dispatch = useDispatch()
+import React, { useEffect } from "react";
+import "../style/home.css";
+import { Swipers } from "./swipes";
+import { Slider } from "./slider";
+import Login from "./login";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import * as types from "../store/types";
+import DefaultButton from "./_microComponents/DefaultButton";
+export function Home() {
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch({
-          type: types.SET_MAIN_PATH,
-          payload: 'home',
-        })
-        
-       }, []);
-          
-    
-   
+  useEffect(() => {
+    dispatch({
+      type: types.SET_MAIN_PATH,
+      payload: "home",
+    });
+  }, []);
 
   return (
     <>
-    <div className="home">
-      <div className="hello-img">
-      <img src={require('../static-img/articles/retro.jpg')} width={'100%'} alt="" />
-    <Link to={'/article'}>
-      <div className="button">Перейти</div>
-    </Link>
+      <div className="home">
+        <div className="hello-img">
+          <img
+            src={require("../static-img/articles/retro.jpg")}
+            width={"100%"}
+            alt=""
+          />
+          <Link to={"/article"}>
+            <div className="button">Перейти</div>
+          </Link>
+        </div>
+        <div className="container-2">
+          <div className="title">Что вас интересует ?</div>
+          <div className="blocks">
+            <div className="item"><img src={require('../static-img/box/jacket.jpg')} alt="" /> <div className="title">куртки</div></div>
+            <div className="item"><img src={require('../static-img/box/sneakers.jpg')} alt="" /> <div className="title">обувь</div></div>
+            <div className="item"><img src={require('../static-img/box/hoodies.jpg')} alt="" /> <div className="title">худи</div></div>
+            <div className="item"><img src={require('../static-img/box/t-shirt.jpg')} alt="" /> <div className="title">футболки</div></div>
+          </div>
+        </div>
       </div>
-    </div>
-
     </>
-  )
+  );
 }
