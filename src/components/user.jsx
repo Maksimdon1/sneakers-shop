d6b19cbb7dff60b1f79a309b4cf9d773d49fe6ee
Maksimdon1 <maksimkozyrev056@gmail.com>
2024-02-01 01:00:09 +0300

    prettier

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,66 +1,54 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../style/user.css";
-import * as types from "../store/types"
+import * as types from "../store/types";
 import LoadingUser from "./user-page-components/loading";
 import { Success } from "./user-page-components/Success";
-import { useDispatch, useSelector } from 'react-redux';
-import {getNewAccessToken} from '../store/actions'
-import {  useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getNewAccessToken } from "../store/actions";
+import { useNavigate } from "react-router-dom";
 
 export function User() {
-  const [ShowLogin, SetShowLogin] = useState()
-  const dispatch = useDispatch()
-  let navigate = useNavigate();
+	const [ShowLogin, SetShowLogin] = useState();
+	const dispatch = useDispatch();
+	let navigate = useNavigate();
 
-  const data = useSelector(
-		state => state.userLogin
-	)
+	const data = useSelector((state) => state.userLogin);
 
+	useEffect(() => {
+		dispatch({
+			type: types.SET_MAIN_PATH,
+			payload: "user",
+		});
+	}, []);
 
- useEffect(() => {
-  dispatch({
-    type: types.SET_MAIN_PATH,
-    payload: 'user',
-  })
-  
- }, []);
+	useEffect(() => {
+		if (data.accessToken && data.userInfo) {
+			dispatch(getNewAccessToken());
+		}
+	}, []);
 
-  useEffect(() => {
+	//  todo add loading effect
+	// if (data.loading) {
+	// 	return (
+	// 		<>
+	// 			<span class="loader"></span>
+	// 		</>
+	// 	);
+	// }
 
-
-
-      
-    if(data.accessToken && data.userInfo){
-       dispatch(getNewAccessToken())
-
-  
-    }
-  
-   
-  
-},[]);
-
-
-
-	const alert =  useSelector(
-		state => state.AlertReducer
-	)
-	if(data.userInfo){
-    return <Success  data = {data.userInfo}/>
+	if (data.userInfo) {
+		return <Success data={data.userInfo} />;
 	}
-  console.log(data)
-  // if (data.loading) {
-  //   return <LoadingUser/>
-  // }
-  if (!data.accessToken) {
+	console.log(data);
+	// if (data.loading) {
+	//   return <LoadingUser/>
+	// }
+	if (!data.accessToken) {
 		dispatch(getNewAccessToken());
 		console.log(data);
 		setTimeout(function () {
 			console.log("eroor exexexexe");
 			navigate("/login");
 		}, 2500);
-  }
-
-  
-
+	}
 }
