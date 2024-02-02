6abaa12d2d6df436fc89db4077960d7368df891e
Maksimdon1 <maksimkozyrev056@gmail.com>
2024-02-02 23:58:19 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,5 +3,4 @@ import axios from 'axios'
 export default axios.create({
 	baseURL: "https://mains-h5w5.onrender.com/server/api", //https://mains-h5w5.onrender.com/server/api    http://192.168.1.4:3002/server/api
 	withCredentials: true,
-	origin : '*',
 });
